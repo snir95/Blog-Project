97ec8a29ff270497a74c3fe71aba5c6986a32bf7
snir95 <snir_60@yahoo.com>
2022-04-04 15:33:31 +0300

    done. v1
    done splitting the code and it works.

--- client/src/Components/Blog/Post.js
+++ client/src/Components/Blog/Post.js
@@ -1,75 +1,55 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-export const CustomCardContainer = ({children}) => {
-  return(
-    <CardContainer>
-      {children}  
-    </CardContainer>
-  )
-}
-export const CustomCard = ({children}) => {
-  return(
-    <Card>
-      {children}  
-    </Card>
-  )
-}
-export const CustomCardHead = ({title}) => {
-  return(
-    <CardHead>{title}:</CardHead>
-  )
-}
-export const CustomCardBody = ({description}) => {
-  return(
-    <CardBody>{description}</CardBody>
-  )
-}
-export const CustomCardFooter = ({date, children}) => {
-  return(
-    <CardFooter>  
-      <p>{date}</p>   
+export const CustomCard = ({ children }) => {
+  return <Card>{children}</Card>;
+};
+export const CustomCardHead = ({ title }) => {
+  return <CardHead>{title}:</CardHead>;
+};
+export const CustomCardBody = ({ description }) => {
+  return <CardBody>{description}</CardBody>;
+};
+export const CustomCardFooter = ({ date, children }) => {
+  return (
+    <CardFooter>
+      <p>{date}</p>
       {children}
     </CardFooter>
-  )
-}
-
-const CardContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-`
+  );
+};
 
 const Card = styled.div`
   align-self: stretch;
   padding: 4px;
-  margin:  10px;
-  min-height:fit-content;
-  border:1px solid rgba(0,0,0,.125);
-  width:25%;
-  
-`
+  margin: 10px;
+  min-height: fit-content;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  background: white;
+  width: 25%;
+`;
 const CardHead = styled.div`
-  align-text:center;
+  align-text: center;
   padding: 0.5rem 1rem;
   margin-bottom: 0;
-  color:black;
-  background-color: rgba(0,0,0,.09);
-  border-bottom: 1px solid rgba(0,0,0,.125);
-  height:fit-content;
-`
+  color: black;
+  background-color: rgba(0, 0, 0, 0.09);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+  height: fit-content;
+`;
 
 const CardBody = styled.div`
   flex: 1 1 auto;
   padding: 1rem 1rem;
-  overflow:scroll;
-  min-height:200px;
-
-`
+  overflow-y: scroll;
+  max-width: 100%;
+  min-height: 200px;
+  max-height: 200px;
+  overflow-wrap: anywhere;
+`;
 const CardFooter = styled.div`
   padding: 0.5rem 1rem;
-  background-color: rgba(0,0,0,.09);
-  border-top: 1px solid rgba(0,0,0,.125);
-  height:fit-content;
-  `
-  
+  background-color: rgba(0, 0, 0, 0.09);
+  border-top: 1px solid rgba(0, 0, 0, 0.125);
+  height: fit-content;
+`;
